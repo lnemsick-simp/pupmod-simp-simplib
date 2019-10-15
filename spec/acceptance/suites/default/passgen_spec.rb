19000c2e65253183b7a1a07a59f761522a8d9ab3
lnemsick-simp <lnemsick.simp@gmail.com>
2019-10-15 15:05:14 -0400

    Updated `simplib::passgen` to run in legacy mode or in a libkv mode.   - When in libkv mode, `simplib:passgen` uses libkv for password persistence.   - libkv mode is enabled by setting `simplib::passgen::libkv` to `true`     in hieradata.   - If you enable libkv mode in a system that already has passwords generated     via the legacy code, currently, **all passwords will be regenerated**.   - Added `libkv_options` parameter to `simplib::passgen` for use in libkv mode.   - Added `gen_timeout_seconds` password option.  Previously this was hardcoded     to 30 seconds. - Added simp-libkv as a module dependency.

--- spec/acceptance/suites/default/passgen_spec.rb
+++ spec/acceptance/suites/default/passgen_spec.rb
@@ -9,38 +9,91 @@ else
 end
 
 describe 'simplib::passgen function' do
+  context 'libkv mode' do
+    let(:hieradata) {{ 'simplib::passgen::libkv' => true }}
 
-  hosts.each do |server|
-    hash_algorithms.each do |hash|
-      (1..10).each do |round|
-        # This test does exercise simplib::passgen, but since simplib::passgen
-        # does not validate a generated password against any pam settings,
-        # (e.g., make sure it passes pwscore), using it for local user passwords
-        # IRL may not be advised.
-        context "when set user 'testuser#{round}#{hash}' password of hash type == #{hash} on #{server}" do
-          let(:manifest) {
-            <<-EOS
-            # generate the most complex password possible and print out so
-            # we have access to it
-            $password = simplib::passgen('testuser-#{hash}-#{round}', {'complexity' => 2, 'complex_only' => true})
-            notify { "password=<${password}> for testuser-#{hash}-#{round}": }
+    hosts.each do |server|
+      context 'test prep' do
+        it 'should enable libkv mode for simplib::passgen' do
+          set_hieradata_on(server, hieradata)
+        end
+      end
 
-            # use hash of the generated password for a user's password
-            $hashed_password = simplib::passgen('testuser-#{hash}-#{round}', {'hash' => '#{hash}', 'complexity' => 2, 'complex_only' => true})
-            user { "testuser#{round}#{hash}":
-              shell => "/bin/bash",
-              managehome => true,
-              password => $password,
+      hash_algorithms.each do |hash|
+        (1..5).each do |round|
+
+          # This test does exercise simplib::passgen, but since simplib::passgen
+          # does not validate a generated password against any pam settings,
+          # (e.g., make sure it passes pwscore), using it for local user passwords
+          # IRL may not be advised.
+          context "when set user 'testuser#{round}#{hash}' password of hash type == #{hash} on #{server}" do
+            let(:manifest) {
+              <<-EOS
+              # generate the most complex password possible and print out so
+              # we have access to it
+              $password = simplib::passgen('testuser-#{hash}-#{round}', {'complexity' => 2, 'complex_only' => true})
+              notify { "password=<${password}> for testuser-#{hash}-#{round}": }
+
+              # use hash of the generated password for a user's password
+              $hashed_password = simplib::passgen('testuser-#{hash}-#{round}', {'hash' => '#{hash}', 'complexity' => 2, 'complex_only' => true})
+              user { "testuser#{round}#{hash}":
+                shell => "/bin/bash",
+                managehome => true,
+                password => $password,
+              }
+              EOS
             }
-            EOS
-          }
 
-          it 'should be able to create user with the generated password and then login with that password' do
-            result = apply_manifest_on(server, manifest)
-            password = result.stdout.match(/password=<(.*)> for testuser/)[1]
+            it 'should be able to create user with the generated password and then login with that password' do
+              result = apply_manifest_on(server, manifest)
+              password = result.stdout.match(/password=<(.*)> for testuser/)[1]
 
-            result = on(server, "echo #{Shellwords.escape(password)} | su -c whoami testuser#{round}#{hash}")
-            expect(result.stdout).to eql("testuser#{round}#{hash}\n")
+              result = on(server, "echo #{Shellwords.escape(password)} | su -c whoami testuser#{round}#{hash}")
+              expect(result.stdout).to eql("testuser#{round}#{hash}\n")
+            end
+          end
+        end
+      end
+    end
+  end
+
+  context 'legacy mode' do
+    let(:hieradata) {{ 'simplib::passgen::libkv' => false }}
+
+    hosts.each do |server|
+      context 'test prep' do
+        it 'should disable libkv mode for simplib::passgen' do
+          set_hieradata_on(server, hieradata)
+        end
+      end
+
+      hash_algorithms.each do |hash|
+        (1..5).each do |round|
+          context "when set user 'testuser#{round}#{hash}' password of hash type == #{hash} on #{server}" do
+            let(:manifest) {
+              <<-EOS
+              # generate the most complex password possible and print out so
+              # we have access to it
+              $password = simplib::passgen('testuser-#{hash}-#{round}', {'complexity' => 2, 'complex_only' => true})
+              notify { "password=<${password}> for testuser-#{hash}-#{round}": }
+
+              # use hash of the generated password for a user's password
+              $hashed_password = simplib::passgen('testuser-#{hash}-#{round}', {'hash' => '#{hash}', 'complexity' => 2, 'complex_only' => true})
+              user { "testuser#{round}#{hash}":
+                shell => "/bin/bash",
+                managehome => true,
+                password => $password,
+              }
+              EOS
+            }
+
+            it 'should be able to create user with the generated password and then login with that password' do
+              result = apply_manifest_on(server, manifest)
+              password = result.stdout.match(/password=<(.*)> for testuser/)[1]
+
+              result = on(server, "echo #{Shellwords.escape(password)} | su -c whoami testuser#{round}#{hash}")
+              expect(result.stdout).to eql("testuser#{round}#{hash}\n")
+            end
           end
         end
       end
